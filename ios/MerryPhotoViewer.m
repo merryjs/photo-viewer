23fe762a0ac8fd6c50e8bf4cdd799ec9e09ece03
bang <sqibang@gmail.com>
2017-07-18 23:59:37 +0700

    doc: add comments

--- ios/MerryPhotoViewer.m
+++ ios/MerryPhotoViewer.m
@@ -9,6 +9,13 @@ RCT_EXPORT_MODULE()
 - (NSDictionary *)constantsToExport {
   return @{@"SHORT" : [NSNumber numberWithDouble:1]};
 }
+
+/**
+ Get root view
+
+ @param rootViewController <#rootViewController description#>
+ @return presented View
+ */
 - (UIViewController *)visibleViewController:(UIViewController *)rootViewController {
   if (rootViewController.presentedViewController == nil) {
     return rootViewController;
@@ -34,8 +41,15 @@ RCT_EXPORT_MODULE()
   return [self visibleViewController:presentedViewController];
 }
 
+
+/**
+ Create an internal method
+
+ @param photos Photos Array
+ */
 - (void)_show:(NSArray *)photos {
   dispatch_async(dispatch_get_main_queue(), ^{
+      
     UIViewController *ctrl =
         [self visibleViewController:[UIApplication sharedApplication].keyWindow.rootViewController];
 
@@ -54,5 +68,14 @@ RCT_EXPORT_MODULE()
   });
 }
 
+
+/**
+ Export for js use
+
+ @param NSArray Photos Array
+
+ */
 RCT_EXPORT_METHOD(show : (NSArray *)photos { [self _show:photos]; });
+
+
 @end
