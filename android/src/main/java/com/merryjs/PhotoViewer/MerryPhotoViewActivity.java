f09ebb85994a4f07d776edac9be5e507214085d2
bang <sqibang@gmail.com>
2017-07-30 00:28:53 +0700

    feat: add circle progress bar #12

--- android/src/main/java/com/merryjs/PhotoViewer/MerryPhotoViewActivity.java
+++ android/src/main/java/com/merryjs/PhotoViewer/MerryPhotoViewActivity.java
@@ -21,49 +21,49 @@ import java.util.Random;
  */
 
 public class MerryPhotoViewActivity extends AppCompatActivity {
-    private MerryPhotoOverlay overlayView;
-    private MerryPhotoViewOptions options;
-    private Activity mActivity;
+	private MerryPhotoOverlay overlayView;
+	private MerryPhotoViewOptions options;
+	private Activity mActivity;
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mActivity = this;
-        setContentView(R.layout.merry_photo_activity);
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mActivity = this;
+		setContentView(R.layout.merry_photo_activity);
 
-        String o = getIntent().getStringExtra("options");
-        options = new Gson().fromJson(o, MerryPhotoViewOptions.class);
+		String o = getIntent().getStringExtra("options");
+		options = new Gson().fromJson(o, MerryPhotoViewOptions.class);
 
-        if (options != null) {
-            //The key argument here must match that used in the other activity
-            showPicker(options);
-        }
-    }
+		if (options != null) {
+			//The key argument here must match that used in the other activity
+			showPicker(options);
+		}
+	}
 
-    protected void showPicker(MerryPhotoViewOptions merryPhotoViewOptions) {
-        // catch exceptions
-        int startPosition = merryPhotoViewOptions.initial;
+	protected void showPicker(MerryPhotoViewOptions merryPhotoViewOptions) {
+		// catch exceptions
+		int startPosition = merryPhotoViewOptions.initial;
 
-        int total = merryPhotoViewOptions.data.length;
+		int total = merryPhotoViewOptions.data.length;
 
-        if (startPosition < 0) {
-            startPosition = 0;
-        }
+		if (startPosition < 0) {
+			startPosition = 0;
+		}
 
-        if (startPosition >= total) {
-            startPosition = total;
-        }
+		if (startPosition >= total) {
+			startPosition = total;
+		}
 
-        ImageViewer.Builder builder = new ImageViewer.Builder(this, merryPhotoViewOptions.data)
-                .setStartPosition(startPosition)
-                .setFormatter(new ImageViewer.Formatter<MerryPhotoData>() {
-                    @Override
-                    public String format(MerryPhotoData o) {
-                        return o.url;
-                    }
-                })
-                .hideStatusBar(merryPhotoViewOptions.hideStatusBar)
-                .setOnDismissListener(getDismissListener());
+		ImageViewer.Builder builder = new ImageViewer.Builder(this, merryPhotoViewOptions.data)
+				.setStartPosition(startPosition)
+				.setFormatter(new ImageViewer.Formatter<MerryPhotoData>() {
+					@Override
+					public String format(MerryPhotoData o) {
+						return o.url;
+					}
+				})
+				.hideStatusBar(merryPhotoViewOptions.hideStatusBar)
+				.setOnDismissListener(getDismissListener());
 //
 //
 //        if (options.get(MerryPhotoViewOptions.Property.IMAGE_MARGIN)) {
@@ -75,89 +75,93 @@ public class MerryPhotoViewActivity extends AppCompatActivity {
 //        }
 //
 //        if (options.get(MerryPhotoViewOptions.Property.IMAGES_ROUNDING)) {
-//            builder.setCustomDraweeHierarchyBuilder(getRoundedHierarchyBuilder());
+		builder.setCustomDraweeHierarchyBuilder(progressBarDrawableBuilder());
 //        }
 //
-        builder.allowSwipeToDismiss(merryPhotoViewOptions.swipeToDismiss);
+		builder.allowSwipeToDismiss(merryPhotoViewOptions.swipeToDismiss);
 //
-        builder.allowZooming(merryPhotoViewOptions.zooming);
+		builder.allowZooming(merryPhotoViewOptions.zooming);
 //
-        overlayView = new MerryPhotoOverlay(this);
-        builder.setOverlayView(overlayView);
-        builder.setImageChangeListener(getImageChangeListener());
+		overlayView = new MerryPhotoOverlay(this);
+		builder.setOverlayView(overlayView);
+		builder.setImageChangeListener(getImageChangeListener());
 //
-        if (options.backgroundColor != null) {
-            builder.setBackgroundColor(Color.parseColor(options.backgroundColor));
-        }
+		if (options.backgroundColor != null) {
+			builder.setBackgroundColor(Color.parseColor(options.backgroundColor));
+		}
 //
 //        if (options.get(MerryPhotoViewOptions.Property.POST_PROCESSING)) {
 //            builder.setCustomImageRequestBuilder(
 //                    ImageViewer.createImageRequestBuilder()
 //                            .setPostprocessor(new GrayscalePostprocessor()));
-//        }
+////        }
 
-        builder.show();
-    }
+		builder.show();
+	}
 
 
-    private ImageViewer.OnImageChangeListener getImageChangeListener() {
-        return new ImageViewer.OnImageChangeListener() {
-            @Override
-            public void onImageChange(int position) {
+	private ImageViewer.OnImageChangeListener getImageChangeListener() {
+		return new ImageViewer.OnImageChangeListener() {
+			@Override
+			public void onImageChange(int position) {
 
-                MerryPhotoData merryPhotoData = options.data[position];
-                String url = merryPhotoData.url;
+				MerryPhotoData merryPhotoData = options.data[position];
+				String url = merryPhotoData.url;
 //                default use url
-                overlayView.setShareContext(url);
+				overlayView.setShareContext(url);
 
-                overlayView.setDescription(merryPhotoData.summary);
-                overlayView.setTitleText(merryPhotoData.title);
+				overlayView.setDescription(merryPhotoData.summary);
+				overlayView.setTitleText(merryPhotoData.title);
 
-                String summaryColor = "#ffffff";
-                String titleColor = "#ffffff";
-                if (options.shareText != null) {
-                    overlayView.setShareText(options.shareText);
-                }
-                if (options.titlePagerColor != null) {
-                    overlayView.setPagerTextColor(options.titlePagerColor);
-                }
+				String summaryColor = "#ffffff";
+				String titleColor = "#ffffff";
+				if (options.shareText != null) {
+					overlayView.setShareText(options.shareText);
+				}
+				if (options.titlePagerColor != null) {
+					overlayView.setPagerTextColor(options.titlePagerColor);
+				}
 
-                overlayView.setPagerText((position + 1) + " / " + options.data.length);
+				overlayView.setPagerText((position + 1) + " / " + options.data.length);
 
-                if (merryPhotoData.titleColor != null) {
-                    titleColor = merryPhotoData.titleColor;
-                }
-                overlayView.setTitleTextColor(titleColor);
-                if (merryPhotoData.summaryColor != null) {
-                    summaryColor = merryPhotoData.summaryColor;
-                }
-                overlayView.setDescriptionTextColor(summaryColor);
+				if (merryPhotoData.titleColor != null) {
+					titleColor = merryPhotoData.titleColor;
+				}
+				overlayView.setTitleTextColor(titleColor);
+				if (merryPhotoData.summaryColor != null) {
+					summaryColor = merryPhotoData.summaryColor;
+				}
+				overlayView.setDescriptionTextColor(summaryColor);
 
-                if (options.shareTextColor != null) {
-                    overlayView.setShareTextColor(options.shareTextColor);
-                }
-            }
-        };
-    }
+				if (options.shareTextColor != null) {
+					overlayView.setShareTextColor(options.shareTextColor);
+				}
+			}
+		};
+	}
 
-    //
-    private ImageViewer.OnDismissListener getDismissListener() {
-        return new ImageViewer.OnDismissListener() {
-            @Override
-            public void onDismiss() {
-                mActivity.finish();
+	//
+	private ImageViewer.OnDismissListener getDismissListener() {
+		return new ImageViewer.OnDismissListener() {
+			@Override
+			public void onDismiss() {
+				mActivity.finish();
 
-            }
-        };
-    }
-//
-//    private GenericDraweeHierarchyBuilder getRoundedHierarchyBuilder() {
-//        RoundingParams roundingParams = new RoundingParams();
-//        roundingParams.setRoundAsCircle(true);
-//
-//        return GenericDraweeHierarchyBuilder.newInstance(getResources())
-//                .setRoundingParams(roundingParams);
-//    }
+			}
+		};
+	}
+
+	private GenericDraweeHierarchyBuilder progressBarDrawableBuilder() {
+		return GenericDraweeHierarchyBuilder.newInstance(getResources())
+				.setProgressBarImage(new CircleProgressBarDrawable());
+	}
+	// private GenericDraweeHierarchyBuilder getRoundedHierarchyBuilder() {
+	//     RoundingParams roundingParams = new RoundingParams();
+	//     roundingParams.setRoundAsCircle(true);
+
+	//     return GenericDraweeHierarchyBuilder.newInstance(getResources())
+	//             .setRoundingParams(roundingParams);
+	// }
 //
 //    private int getRandomColor() {
 //        Random random = new Random();
--- example/android/app/src/main/java/com/photoviewerexample/MainApplication.java
+++ example/android/app/src/main/java/com/photoviewerexample/MainApplication.java
@@ -2,6 +2,7 @@ package com.photoviewerexample;
 
 import android.app.Application;
 
+import com.facebook.drawee.backends.pipeline.DraweeConfig;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.imagepipeline.core.ImagePipelineConfig;
 import com.facebook.imagepipeline.decoder.SimpleProgressiveJpegConfig;
@@ -46,6 +47,9 @@ public class MainApplication extends Application implements ReactApplication {
                 .setResizeAndRotateEnabledForNetwork(true)
                 .setDownsampleEnabled(true)
                 .build();
-        Fresco.initialize(this, config);
+        DraweeConfig draweeConfig = DraweeConfig.newBuilder()
+                .setDrawDebugOverlay(true)
+                .build();
+        Fresco.initialize(this, config, draweeConfig);
     }
 }
