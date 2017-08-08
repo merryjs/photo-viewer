57ed1fc1ba5f364a967b9a660d9692587202a28a
bang <sqibang@gmail.com>
2017-08-08 22:31:57 +0700

    chore: remove build file

--- ios/Carthage/Build/iOS/MBCircularProgressBar.framework/Headers/MBCircularProgressBar-umbrella.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "MBCircularProgressBarLayer.h"
-#import "MBCircularProgressBarView.h"
-
-FOUNDATION_EXPORT double MBCircularProgressBarVersionNumber;
-FOUNDATION_EXPORT const unsigned char MBCircularProgressBarVersionString[];
-
