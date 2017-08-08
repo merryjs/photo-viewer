d3baecf10307f861dea2fa8b4e2d42fd6a197708
bang <sqibang@gmail.com>
2017-08-08 15:46:33 +0700

    chore: clean up

--- android/src/main/java/com/merryjs/PhotoViewer/MerryPhotoData.java
+++ android/src/main/java/com/merryjs/PhotoViewer/MerryPhotoData.java
@@ -3,18 +3,7 @@ package com.merryjs.PhotoViewer;
 /**
  * Created by bang on 26/07/2017.
  */
-//
-//export interface Photo {
-//    url: string;
-//    title: string;
-//    summary: string;
-//}
-//
-//export interface PhotoViewerOptions {
-//    data: string[];
-//    titleColor?: string;
-//    summaryColor?: string;
-//}
+
 public class MerryPhotoData {
     public String url;
     public String title;
