f99435d232ea6369ec0ec02a7cb14dbbc3ad1f95
bang88 <sqibang@gmail.com>
2021-09-26 10:13:09 +0800

    fix: android

--- android/src/main/java/com/stfalcon/frescoimageviewer/drawee/ZoomableDraweeView.java
+++ android/src/main/java/com/stfalcon/frescoimageviewer/drawee/ZoomableDraweeView.java
@@ -141,6 +141,11 @@ public class ZoomableDraweeView extends SimpleDraweeView implements IAttacher {
     }
 
     @Override
+    public void setOrientation(int orientation) {
+
+    }
+
+    @Override
     public void setZoomTransitionDuration(long duration) {
         attacher.setZoomTransitionDuration(duration);
     }
